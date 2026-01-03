0fc127cad6223742ed58b5d4234609616346a71b
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-01-03 20:04:21 +0600

    Format code with prettier

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 // DOM Elements:
-let library = [];
-const showcase = document.querySelector('.showcase');
-const addBook = document.querySelector('.add-book');
-const dialog = document.querySelector('dialog');
-const dialogClose = document.querySelector('dialog .btn-close');
-const dialogSubmit = document.querySelector('dialog button[type=Submit]');
+const library = [];
+const showcase = document.querySelector(".showcase");
+const addBookBtn = document.querySelector(".add-book");
+const dialog = document.querySelector("dialog");
+const dialogClose = document.querySelector("dialog .btn-close");
+const dialogSubmit = document.querySelector("dialog button[type=Submit]");
 
 // Event Listeners:
-addBook.addEventListener('click', () => {
+addBookBtn.addEventListener("click", () => {
   dialog.showModal();
-})
+});
 
-dialog.addEventListener('close', () => {
+dialog.addEventListener("close", () => {
   console.log(dialog.returnValue);
-})
+});
 
-dialogClose.addEventListener('click', () => {
+dialogClose.addEventListener("click", () => {
   dialog.close();
-})
+});
 
-dialogSubmit.addEventListener('click', (e) => {
+dialogSubmit.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const title = document.querySelector('#title');
-  const author = document.querySelector('#author');
-  const pages = document.querySelector('#pages');
-  const read = document.querySelector('#read');
-  addToLibrary(title.value, author.value, pages.value, read.value);
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const read = document.querySelector("#read");
+  addToLibrary(title.value, author.value, pages.value, read.checked);
   displayBook(library[library.length - 1]);
 
-  [title.value, author.value, pages.value, read.value] = ['', '', '', ''];
+  [title.value, author.value, pages.value, read.checked] = ["", "", "", ""];
   dialog.close();
-})
+});
 
 // Constructor & Regular Function Declarations:
 function Book(title, author, pages) {
@@ -40,12 +40,12 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-Book.prototype.toggleReadStatus = function() {
+Book.prototype.toggleReadStatus = function () {
   this.read = !this.read;
   console.log(this);
-}
+};
 
-function addToLibrary(title, author, pages, read=false) {
+function addToLibrary(title, author, pages, read = false) {
   // take params, create a book then store it in the array
   const book = new Book(title, author, pages);
   book.id = crypto.randomUUID();
@@ -54,31 +54,31 @@ function addToLibrary(title, author, pages, read=false) {
 }
 
 function displayBook(book) {
-  const title = document.createElement('div');
+  const title = document.createElement("div");
   title.textContent = book.title;
-  title.classList.add('title');
+  title.classList.add("title");
 
-  const author = document.createElement('div');
+  const author = document.createElement("div");
   author.textContent = book.author;
-  author.classList.add('author');
+  author.classList.add("author");
 
-  const pages = document.createElement('div');
+  const pages = document.createElement("div");
   pages.textContent = `${book.pages} pages`;
-  pages.classList.add('pages');
+  pages.classList.add("pages");
 
-  const btnDelete = document.createElement('button');
-  btnDelete.textContent = 'Delete';
-  btnDelete.classList.add('btn-delete');
-  btnDelete.addEventListener('click', deleteBook);
+  const btnDelete = document.createElement("button");
+  btnDelete.textContent = "Delete";
+  btnDelete.classList.add("btn-delete");
+  btnDelete.addEventListener("click", deleteBook);
 
-  const btnStatus = document.createElement('button');
-  btnStatus.textContent = book.read ? 'Unread' : 'Read';
-  btnStatus.classList.add('btn-read-status');
-  btnStatus.addEventListener('click', toggleStatus);
+  const btnStatus = document.createElement("button");
+  btnStatus.textContent = book.read ? "Unread" : "Read";
+  btnStatus.classList.add("btn-read-status");
+  btnStatus.addEventListener("click", toggleStatus);
 
-  const bookElement = document.createElement('div');
-  bookElement.setAttribute('data-id', book.id);
-  bookElement.classList.add('book');
+  const bookElement = document.createElement("div");
+  bookElement.setAttribute("data-id", book.id);
+  bookElement.classList.add("book");
   bookElement.appendChild(title);
   bookElement.appendChild(author);
   bookElement.appendChild(pages);
@@ -88,34 +88,34 @@ function displayBook(book) {
 }
 
 function displayBooks() {
-  library.forEach(book => {
+  library.forEach((book) => {
     displayBook(book);
-  })
+  });
 }
 
 function toggleStatus(e) {
   const bookEl = e.target.parentNode;
-  const bookID = bookEl.getAttribute('data-id');
+  const bookID = bookEl.getAttribute("data-id");
 
-  library.forEach(book => {
+  library.forEach((book) => {
     console.log(book.id, bookID);
     if (book.id === bookID) {
       book.toggleReadStatus();
-      e.target.textContent = book.read ? 'Unread' : 'Read';
+      e.target.textContent = book.read ? "Unread" : "Read";
       console.log(book);
     }
-  })
+  });
 }
 
 function deleteBook(e) {
   const bookEl = e.target.parentNode;
-  const bookID = bookEl.getAttribute('data-id');
+  const bookID = bookEl.getAttribute("data-id");
 
-  library = library.filter(book => book.id !== bookID);
+  library = library.filter((book) => book.id !== bookID);
   bookEl.remove();
 }
 
 // Taste Codes:
-addToLibrary('Who Cares?', 'Hu Nouz', 347);
+addToLibrary("Who Cares?", "Hu Nouz", 347);
 
 displayBooks();
